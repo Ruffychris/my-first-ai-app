68874a4a84b63775ddbffb3fa0e5b217393e3f5f
Ruffychris <christian.rufus23@gmail.com>
2026-07-25 20:01:51 +0000

    week 1 task 1 completed

--- week1/hello.py
+++ week1/hello.py
@@ -7,9 +7,13 @@ Run it with:   python week1/hello.py
 """
 
 # TODO: print a greeting
-
+print ("Good evening")
 
 # TODO: create a variable with your name, and use it in a message
-
+name = "Rufus"
 
 # TODO: try an f-string  ->  print(f"Hello, {name}!")
+message = "Good evening, I am "
+
+
+print(message + name)
